928d429ec0c9aa17a5e22d9981f32951ea218be0
CeramicTitan <ceramictitandev@gmail.com>
2013-01-04 22:39:09 +1100

    Didn't recognise code...

--- src/me/CeramicTitan/InstantBreak/InstantBreak.java
+++ src/me/CeramicTitan/InstantBreak/InstantBreak.java
@@ -1,5 +1,25 @@
-package me.CeramicTitan.InstantBreak;
+package me.ceramictitan.Insta;
 
-public class InstantBreak {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Break extends JavaPlugin implements Listener {
+
+	public void onDisable() {
+
+	}
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onBlockDamage(BlockDamageEvent event) {
+		if (event.getPlayer().hasPermission("insta.break")) {
+			event.setInstaBreak(true);
+		}
+	}
 
 }
